dd0bab92f25023798c580b579dd5d5294c93c463
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-06-12 15:40:59 -0700

    Function as a constructor example 2

---
+++ class-function-as-a-constructor2.js
@@ -0,0 +1,13 @@
+function Person(name, age, address, birth) {
+	// Properties
+	this.name = name;
+	this.age = age;
+	this.address = address;
+	this.birth = birth;
+}
+
+// Instantiate ab object
+var person1 = new Person('Cristina', 28, 'LA, Ca', '20/January/1989');
+
+console.log(person1.name, person1.age, person1.address, person1.birth);
+
